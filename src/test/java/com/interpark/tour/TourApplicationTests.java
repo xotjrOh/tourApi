4692847f3158b91bb7c7e7578fd9cf72e4b05b05
taeseokOh <xotjr8052@gmail.com>
2022-11-11 17:49:03 +0900

    first

---
+++ src/test/java/com/interpark/tour/TourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interpark.tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
